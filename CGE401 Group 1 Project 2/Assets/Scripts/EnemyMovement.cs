05cc236d0fb823ed483c27d49a4ae4f8618bf7f3
Quetzalberry <denverheneghan@gmail.com>
2023-11-11 19:52:45 -0600

    Temp Sprites

---
+++ CGE401 Group 1 Project 2/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Text youLose;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            youLose.enabled = true;
+        }
+    }
+}
