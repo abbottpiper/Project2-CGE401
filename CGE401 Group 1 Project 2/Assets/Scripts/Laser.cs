07c216711cc30f52e390e09b66ab8230015e8ed6
abbottpiper <143674663+abbottpiper@users.noreply.github.com>
2023-11-26 17:07:33 -0600

    Level3 Lasers

--- CGE401 Group 1 Project 2/Assets/Scripts/Laser.cs
+++ CGE401 Group 1 Project 2/Assets/Scripts/Laser.cs
@@ -7,44 +7,62 @@ public class Laser : MonoBehaviour
 	public GameObject laser1;
 	public GameObject laser2;
 	public GameObject laser3;
+	public GameObject laser4;
+	public GameObject laser5;
+	public GameObject laser6;
+	public GameObject laser7;
+	public GameObject laser8;
+	public GameObject laser9;
+	public GameObject laser10;
+	public GameObject laser11;
+	public GameObject laser12;
 
-	public bool laserStart;
-
-    void Start()
-    {
-		laserStart = true;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-		StartCoroutine(LaserTimer());
-    }
-
-	IEnumerator LaserTimer()
+	public void OnCollisionEnter2D(Collision2D collision)
 	{
-		yield return new WaitForSeconds(2);
+		if (collision.gameObject.tag == "Button")
+        {
+			laser2.SetActive(false);
+			laser3.SetActive(false);
+		}
 
-		laser1.SetActive(true);
-		laser2.SetActive(true);
-		laser3.SetActive(true);
+		if (collision.gameObject.tag == "Button1")
+        {
+			laser1.SetActive(false);
+		}
 
-		yield return new WaitForSeconds(2);
+		if (collision.gameObject.tag == "Button2")
+        {
+			laser4.SetActive(false);
+		}
 
-		laser1.SetActive(false);
-		laser2.SetActive(false);
-		laser3.SetActive(false);
+		if (collision.gameObject.tag == "Button3")
+        {
+			laser5.SetActive(false);
+		}
 
-		yield return new WaitForSeconds(2);
+		if (collision.gameObject.tag == "Button4")
+        {
+			laser6.SetActive(false);
+		}
 
-		laser1.SetActive(true);
-		laser2.SetActive(true);
-		laser3.SetActive(true);
+		if (collision.gameObject.tag == "Button5")
+        {
+			laser7.SetActive(false);
+		}
 
-		yield return new WaitForSeconds(2);
+		if (collision.gameObject.tag == "Button6")
+        {
+			laser8.SetActive(false);
+		}
 
-		laser1.SetActive(false);
-		laser2.SetActive(false);
-		laser3.SetActive(false);
+		if (collision.gameObject.tag == "Button7")
+        {
+			laser9.SetActive(false);
+		}
+
+		if (collision.gameObject.tag == "Button8")
+        {
+			laser10.SetActive(false);
+		}
 	}
 }
