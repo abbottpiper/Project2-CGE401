7f2da2009215588b38012873f682eedc1a375d5e
abbottpiper <143674663+abbottpiper@users.noreply.github.com>
2023-11-25 18:47:50 -0600

    Laser

--- CGE401 Group 1 Project 2/Assets/Scripts/Laser.cs
+++ CGE401 Group 1 Project 2/Assets/Scripts/Laser.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
-    private bool start; 
-
 	public GameObject laser;
 
     void Start()
     {
-		start = true;   
+ 
     }
 
     // Update is called once per frame
     void Update()
     {
-        while(start == true)
-		{
-			laser.SetActive(false);
-		}
+
     }
 
 	IEnumerator LaserTimer()
