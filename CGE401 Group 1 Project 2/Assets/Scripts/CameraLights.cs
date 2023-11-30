e1cecadf5fd165d5e47ecc1d9fbd251328d7d57a
Quetzalberry <143647124+Quetzalberry@users.noreply.github.com>
2023-11-30 16:27:04 -0600

    Camera script

--- CGE401 Group 1 Project 2/Assets/Scripts/CameraLights.cs
+++ CGE401 Group 1 Project 2/Assets/Scripts/CameraLights.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class CameraLights : MonoBehaviour
 {
+    int num = 0;
+    int wireAmount = 0;
+
+    public GameObject Light1;
+    public GameObject Light2;
+    public GameObject Light3;
+    public GameObject Light4;
+    public GameObject Light5;
+
     void Start()
     {
         
@@ -11,6 +20,39 @@ public class CameraLights : MonoBehaviour
 
     void Update()
     {
-        
+        if (num == 1)
+        {
+            Destroy(Light1);
+        }
+
+        if (num == 2)
+        {
+            Destroy(Light2);
+        }
+
+        if (num == 3)
+        {
+            Destroy(Light3);
+        }
+
+        if (num == 4)
+        {
+            Destroy(Light4);
+        }
+
+        if (num == 5)
+        {
+            Destroy(Light5);
+        }
+    }
+
+    public void Wires()
+    {
+        wireAmount++;
+
+        if (wireAmount == 5)
+        {
+            num++;
+        }
     }
 }
