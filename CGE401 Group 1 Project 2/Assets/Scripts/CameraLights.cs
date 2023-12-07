92fe82407ac23d91cc5303178229c743bc174531
Quetzalberry <143647124+Quetzalberry@users.noreply.github.com>
2023-12-07 16:28:25 -0600

    camera

--- CGE401 Group 1 Project 2/Assets/Scripts/CameraLights.cs
+++ CGE401 Group 1 Project 2/Assets/Scripts/CameraLights.cs
@@ -13,31 +13,42 @@ public class CameraLights : MonoBehaviour
     public GameObject Light4;
     public GameObject Light5;
 
+    public GameObject WireBoxOutside;
+    public GameObject WireBoxOutside1;
+    public GameObject WireBoxOutside2;
+    public GameObject WireBoxOutside3;
+    public GameObject WireBoxOutside4;
+
     void Update()
     {
         if (num == 1)
         {
             Destroy(Light1);
+            Destroy(WireBoxOutside);
         }
 
         if (num == 2)
         {
             Destroy(Light2);
+            Destroy(WireBoxOutside1);
         }
 
         if (num == 3)
         {
             Destroy(Light3);
+            Destroy(WireBoxOutside2);
         }
 
         if (num == 4)
         {
             Destroy(Light4);
+            Destroy(WireBoxOutside3);
         }
 
         if (num == 5)
         {
             Destroy(Light5);
+            Destroy(WireBoxOutside4);
         }
     }
 
